666bfc1b377783cae57e4fac793bbd8db2b62d3c
Альберт Гадиев <albertgadiev@yandex.com>
2025-03-01 19:16:26 +0600

    added sh file for start in linux

---
+++ start.sh
@@ -0,0 +1,7 @@
+g++ Game.cpp main.cpp Math.cpp OpenGlExtensions.cpp Physics.cpp Renderer.cpp ShaderManager.cpp TextureManager.cpp Utils.cpp BoneAnimatedModel.cpp ObjLoader.cpp cmakeaudioplayer/src/AudioPlayer.cpp TextModel.cpp Inventory.cpp -o sdl_app -O2 -std=c++17 \
+-I cmakeaudioplayer/include \
+$(pkg-config --cflags --libs sdl2 gl) \
+$(pkg-config --cflags --libs vorbis vorbisfile ogg) \
+-lopenal
+
+./sdl_app
