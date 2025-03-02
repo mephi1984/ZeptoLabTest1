650927498c37321803779a2e5d4f8e4506ad38c8
maka70vv <25.makarovv@gmail.com>
2025-03-03 00:22:14 +0600

    fixed cubs sorting

--- QuestScripts.cpp
+++ QuestScripts.cpp
@@ -15,6 +15,7 @@ namespace ZL
         {
             if (gom.bearName.compare("TOM") == 0) {
                 gInventoryMap.clear();
+                gom.objects_in_inventory = 0;
 //                std::this_thread::sleep_for(std::chrono::seconds(1));
                 gom.switch_room(1);
             }
