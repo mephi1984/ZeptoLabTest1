4fd09212dd6c8af77a34e16e06ccb6f7bb7b52aa
maka70vv <25.makarovv@gmail.com>
2025-03-02 10:01:05 +0600

    tested game logic

--- QuestScripts.cpp
+++ QuestScripts.cpp
@@ -9,12 +9,13 @@ namespace ZL
     std::function<void(GameObjectManager&, size_t)> createRoom1Logic()
     {
         return [](GameObjectManager& gom, size_t ms)
+//      Simple test logic
         {
-//            if (gom.inventory.HasItem("book")) {
-//                std::cout << "[Room 1] Игрок поднял книгу!\n";
-//
-//                 gom.switch_room(1);
-//            }
+            if (GetItemByName("book")) {
+                std::cout << "[Room 1] Игрок поднял книгу!\n";
+
+                 gom.switch_room(1);
+            }
         };
     }
 
